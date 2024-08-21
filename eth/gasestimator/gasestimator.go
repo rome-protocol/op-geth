6b14d73b8975f76bf77639fbf13770a575868f88
ivan-loboda-1990 <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>
2024-08-21 17:46:55 +0400

    remove unused imports

--- eth/gasestimator/gasestimator.go
+++ eth/gasestimator/gasestimator.go
@@ -21,9 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
+	
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
