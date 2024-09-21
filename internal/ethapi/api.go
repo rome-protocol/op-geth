74948c6b124effc94d93782c326d9f7832b8ed16
Vikram B. <vikram@romeprotocol.com>
2024-09-22 02:13:48 +0530

    feat: add gas to contract constructor

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1787,7 +1787,7 @@ func AccessList(ctx context.Context, b Backend, blockNrOrHash rpc.BlockNumberOrH
 		tracer := logger.NewAccessListTracer(accessList, args.from(), to, precompiles)
 		config := vm.Config{Tracer: tracer, NoBaseFee: true}
 		vmenv := b.GetEVM(ctx, msg, statedb, header, &config, nil)
-		res, err := core.ApplyMessage(vmenv, msg, new(core.GasPool).AddGas(1000000000), 0)
+		res, err := core.ApplyMessage(vmenv, msg, new(core.GasPool).AddGas(10000000000), 0)
 		if err != nil {
 			return nil, 0, nil, fmt.Errorf("failed to apply transaction: %v err: %v", args.toTransaction().Hash(), err)
 		}
