31aae6130323e6b2b96240a15be07d4dd3ebaa42
Vikram B. <vikram@romeprotocol.com>
2024-09-22 00:10:39 +0530

    Update gaspool.go

--- core/gaspool.go
+++ core/gaspool.go
@@ -19,6 +19,8 @@ package core
 import (
 	"fmt"
 	"math"
+
+	"github.com/ethereum/go-ethereum/log"
 )
 
 // GasPool tracks the amount of gas available during execution of the transactions
@@ -38,6 +40,7 @@ func (gp *GasPool) AddGas(amount uint64) *GasPool {
 // available and returns an error otherwise.
 func (gp *GasPool) SubGas(amount uint64) error {
 	if uint64(*gp) < amount {
+		log.Info("sub gas", "gas pool", uint64(*gp), "amount", amount)
 		return ErrGasLimitReached
 	}
 	*(*uint64)(gp) -= amount
