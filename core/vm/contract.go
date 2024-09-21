58b9e46f4ca2ba0058f3d5af17efc7f4a8cbdaa1
Vikram B. <vikram@romeprotocol.com>
2024-09-22 01:59:48 +0530

    Update contract.go

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -75,7 +75,7 @@ func NewContract(caller ContractRef, object ContractRef, value *big.Int, gas uin
 
 	// Gas should be a pointer so it can safely be reduced through the run
 	// This pointer will be off the state transition
-	c.Gas = 10000000000000000
+	c.Gas = gas
 	// ensures a value is set
 	c.value = value
 
