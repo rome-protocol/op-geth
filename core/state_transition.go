6b84a41f3acb462e18ca795b475778787d59d28f
Vikram B. <vikram@romeprotocol.com>
2024-10-01 15:19:11 +0530

    Update state_transition.go

--- core/state_transition.go
+++ core/state_transition.go
@@ -488,7 +488,7 @@ func (st *StateTransition) innerTransitionDb(romeGasUsed uint64) (*ExecutionResu
 		// the coinbase when simulating calls.
 	} else {
 		fee := new(big.Int).SetUint64(romeGasUsed)
-		fee.Mul(fee, effectiveTip)
+		// fee.Mul(fee, effectiveTip)
 		zeroAddress := common.Address{}
 		if st.evm.Context.Coinbase != zeroAddress {
 			log.Info("coinbase", "address", st.evm.Context.Coinbase)
