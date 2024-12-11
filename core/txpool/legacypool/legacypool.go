54749ee25bb98273a49363e01a650efa8ca03fe3
rukimedo1991 <vikram@romeprotocol.com>
2024-12-11 22:01:11 +0530

    update logs on tx pool

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -380,14 +380,18 @@ func (pool *LegacyPool) loop() {
 			pool.mu.Lock()
 			for addr := range pool.queue {
 				// Skip local transactions from the eviction mechanism
+				log.Info("inside tx pool", "addr", addr)
 				if pool.locals.contains(addr) {
+					log.Info("inside locals pool", "addr", addr)
 					continue
 				}
 				// Any non-locals old enough should be removed
+				log.Info("crossed locals pool", "Lifetime", pool.config.Lifetime)
 				if time.Since(pool.beats[addr]) > pool.config.Lifetime {
 					list := pool.queue[addr].Flatten()
 					for _, tx := range list {
 						pool.removeTx(tx.Hash(), true, true)
+						log.Info("removed tx", "hash", tx.Hash())
 					}
 					queuedEvictionMeter.Mark(int64(len(list)))
 				}
