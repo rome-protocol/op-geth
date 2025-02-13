9ff575c6eec0d43c714133a31939fbb06f8f0e5b
rukimedo1991 <vikram@romeprotocol.com>
2025-02-14 03:17:12 +0530

    Update evm.go

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -534,7 +534,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 // Create creates a new contract using code as deployment code.
 func (evm *EVM) Create(caller ContractRef, code []byte, gas uint64, value *big.Int) (ret []byte, contractAddr common.Address, leftOverGas uint64, err error) {
 	contractAddr = crypto.CreateAddress(caller.Address(), evm.StateDB.GetNonce(caller.Address()))
-	log.Info("create contractAddr", contractAddr)
+	log.Info("create contractAddr", contractAddr, "nonce", evm.StateDB.GetNonce(caller.Address()), "caller", caller.Address().String())
 	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr, CREATE)
 }
 
