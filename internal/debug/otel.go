cfb4536abb92491557aa696bd03ad54229430bbe
rukimedo1991 <vikram@romeprotocol.com>
2025-04-11 21:30:37 +0530

    add otel package

---
+++ internal/debug/otel.go
@@ -0,0 +1,72 @@
+package debug
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func initTracer() func() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	res, err := newResource(ctx)
+	reportErr(err, "failed to create res")
+
+	conn, err := grpc.DialContext(ctx, "localhost:4317", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	reportErr(err, "failed to create gRPC connection to collector")
+
+	// Set up a trace exporter
+	traceExporter, err := newExporter(ctx, conn)
+	reportErr(err, "failed to create trace exporter")
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := newTraceProvider(res, batchSpanProcessor)
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		reportErr(tracerProvider.Shutdown(ctx), "failed to shutdown TracerProvider")
+		cancel()
+	}
+}
+
+func newTraceProvider(res *resource.Resource, bsp sdktrace.SpanProcessor) *sdktrace.TracerProvider {
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	return tracerProvider
+}
+
+func newExporter(ctx context.Context, conn *grpc.ClientConn) (*otlptrace.Exporter, error) {
+	return otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+}
+
+func newResource(ctx context.Context) (*resource.Resource, error) {
+	return resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceNameKey.String("otel-otlp-go-service"),
+			attribute.String("application", "otel-otlp-go-app"),
+		),
+	)
+}
+
+func reportErr(err error, message string) {
+	if err != nil {
+		log.Printf("%s: %v", message, err)
+	}
+}
