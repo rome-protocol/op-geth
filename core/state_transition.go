c7eaf6af24750b91393074366bcc0c4b4d6d4a96
rukimedo1991 <vikram@romeprotocol.com>
2025-05-08 10:39:36 +0530

    remove effective tip

--- core/state_transition.go
+++ core/state_transition.go
@@ -400,18 +400,12 @@ func (st *StateTransition) innerTransitionDb(romeGasUsed uint64) (*ExecutionResu
 		}, nil
 	}
 
-	effectiveTip := msg.GasPrice
-	if rules.IsLondon {
-		effectiveTip = msg.GasTipCap
-	}
-
 	if st.evm.Config.NoBaseFee && msg.GasFeeCap.Sign() == 0 && msg.GasTipCap.Sign() == 0 {
 		// Skip fee payment when NoBaseFee is set and the fee fields
 		// are 0. This avoids a negative effectiveTip being applied to
 		// the coinbase when simulating calls.
 	} else {
 		fee := new(big.Int).SetUint64(romeGasUsed)
-		fee.Mul(fee, effectiveTip)
 		zeroAddress := common.Address{}
 		if st.evm.Context.Coinbase != zeroAddress {
 			st.state.AddBalance(st.evm.Context.Coinbase, fee)
