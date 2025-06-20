6c3364e96d540068257bd0020f25e017faa5d678
rukimedo1991 <vikram@romeprotocol.com>
2025-06-20 19:56:44 +0400

    capture dirty storage separately

--- core/state_processor.go
+++ core/state_processor.go
@@ -133,6 +133,7 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 		return nil, err
 	}
 
+	statedb.CaptureDirtyStorage()
 	// Calculate the state footprint after VM execution
 	vmState := statedb.CalculateTxFootPrint()
 
