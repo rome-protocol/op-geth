8b017db7bd4b073f33d0de36df2fa89523e401c2
rukimedo1991 <vikram@romeprotocol.com>
2025-09-24 22:50:32 +0530

    Update state_processor.go

--- core/state_processor.go
+++ core/state_processor.go
@@ -134,16 +134,16 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 	}
 
 	// Calculate the state footprint after VM execution
-	if footPrint != "" && footPrint != "0x0" {
-		vmState, logs := statedb.CalculateTxFootPrint()
-
-		if vmState != common.HexToHash(footPrint) {
-			if err := log.FlushLogs(logs); err != nil {
-				log.Error("failed to flush logs", "error", err)
-			}
-			log.Warn("state footprint mismatch: expected %s, got %s", footPrint, vmState)
+	// if footPrint != "" && footPrint != "0x0" {
+	vmState, logs := statedb.CalculateTxFootPrint()
+	if vmState != common.HexToHash(footPrint) {
+		if err := log.FlushLogs(logs); err != nil {
+			log.Error("failed to flush logs", "error", err)
 		}
+
+		log.Info("state footprint mismatch: expected %s, got %s", footPrint, vmState)
 	}
+	// }
 
 	// Update the state with pending changes.
 	var root []byte
