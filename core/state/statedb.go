bafa015ec7fe84126637ec05a3cd6460f9dd8191
rukimedo1991 <vikram@romeprotocol.com>
2025-11-25 15:50:40 +0530

    Revert "TouchAccountForFootprint"
    This reverts commit f9b2ccd518907259f80152730b413e2eea29c79a.

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -702,15 +702,6 @@ func (s *StateDB) CreateAccount(addr common.Address) {
 	}
 }
 
-// TouchAccountForFootprint marks an account as touched for footprint calculation purposes.
-// This is used to track accounts whose code is executed via CALL even if no state changes occur.
-func (s *StateDB) TouchAccountForFootprint(addr common.Address) {
-	obj := s.getStateObject(addr)
-	if obj != nil {
-		obj.touch()
-	}
-}
-
 // Copy creates a deep, independent copy of the state.
 // Snapshots of the copied state cannot be applied to the copy.
 func (s *StateDB) Copy() *StateDB {
