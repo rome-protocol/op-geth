41b84eb8669fbdc1770acb38838b6b983f9a8f2f
rukimedo1991 <vikram@romeprotocol.com>
2025-12-10 19:02:40 +0530

    build issue

--- eth/state_accessor.go
+++ eth/state_accessor.go
@@ -241,7 +241,7 @@ func (eth *Ethereum) stateAtTransaction(ctx context.Context, block *types.Block,
 		// Assemble the transaction call message and return if the requested offset
 		msg, _ := core.TransactionToMessage(tx, signer, block.BaseFee())
 		txContext := core.NewEVMTxContext(msg)
-		context := core.NewEVMBlockContext(block.Header(), eth.blockchain, nil, eth.blockchain.Config(), statedb, nil, nil)
+		context := core.NewEVMBlockContext(block.Header(), eth.blockchain, nil, eth.blockchain.Config(), statedb, nil)
 		if idx == txIndex {
 			return msg, context, statedb, release, nil
 		}
