448045a8f80de3f94345d61a79fcf5a400000a3c
rukimedo1991 <vikram@romeprotocol.com>
2025-12-15 16:50:08 +0530

    eip 158

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -470,9 +470,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	// Create a new account on the state
 	snapshot := evm.StateDB.Snapshot()
 	evm.StateDB.CreateAccount(address)
-	if evm.chainRules.IsEIP158 {
-		evm.StateDB.SetNonce(address, 1)
-	}
+
 	evm.Context.Transfer(evm.StateDB, caller.Address(), address, value)
 
 	// Initialise a new contract and set the code that is to be used by the EVM.
